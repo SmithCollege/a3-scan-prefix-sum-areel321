f911fb7173037d3fc748731f53bd5c6e9c3e1417
Allison <areel@smith.edu>
2024-10-24 10:46:39 -0400

    edits

--- cpuSingleThread.c
+++ cpuSingleThread.c
@@ -1,7 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
 
-#define SIZE 128
+#define SIZE 1024
+
+double get_clock() {
+	struct timeval tv; int ok;
+	ok = gettimeofday(&tv, (void *) 0);
+	if (ok<0){
+		printf("gettimeofday error\n");
+	}
+	return (tv.tv_sec*1.0+tv.tv_usec*1.0E-6);
+}
+
 
 int main() {
         //output: sum of all prev elements
@@ -9,12 +20,22 @@ int main() {
         //allocate memory
         int* input = malloc(sizeof(int) * SIZE);
         int* output = malloc(sizeof(int) * SIZE);
+        double *times = malloc(sizeof(double) * SIZE);
+
+        double t0 = get_clock();
+        for (int i=0; i<SIZE; i++){
+        	times[i] = get_clock();
+        }
+        double t1 = get_clock();
+        printf("time per call: %f nx\n", (1000000000.0 * (t1-t0)/SIZE));
+        
 
         //initialize inputs
         for (int i=0; i<SIZE; i++) {
                 input[i]=1;
         }
 
+		double start = get_clock();
         //do the scan
         for (int i=0; i<SIZE;i++) {
                 int value = 0;
@@ -23,16 +44,22 @@ int main() {
                 }
                 output[i] = value;
         }
+        double end = get_clock();
 
         //check results
-        for (int i=0; i<SIZE; i++) {
+        /*for (int i=0; i<SIZE; i++) {
                 printf("%d ", output[i]);
         }
-        printf("\n");
+        printf("\n");*/
+
+        printf("start: %f, end: %f\n", start, end);
+
+        
 
         //free mem
         free(input);
         free(output);
+        free(times);
 
         return 0;
 }
