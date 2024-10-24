da2a88093f0439789d3eedd1a6a13f7dd8b9412d
Allison <areel@smith.edu>
2024-10-24 10:46:58 -0400

    edits

--- naive.cu
+++ naive.cu
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <math.h>
+#include <sys/time.h>
 
 #define SIZE 128
+
 
 //kernel function to perform prefix sum with multiple threads
 __global__
@@ -20,11 +22,29 @@ void prefixsum(int *input, int *output){
 
 }
 
+double get_clock() {
+        struct timeval tv; int ok;
+        ok = gettimeofday(&tv, (void *) 0);
+        if (ok<0){
+                printf("gettimeofday error");
+        }
+        return (tv.tv_sec * 1.0 + tv.tv_usec * 1.0E-6);
+}
+
 
 int main(void){
 
         //define and allocate input and output arrays
         int *input, *output;
+	int *times;
+  	cudaMallocManaged(&times, SIZE*sizeof(double));
+
+	double t0 = get_clock();
+  	for (int i=0; i<SIZE; i++){
+            times[i] = get_clock();
+  	}
+  	double t1 = get_clock();
+  	printf("time per call: %f nx\n", (1000000000.0 * (t1-t0)/SIZE));		
 
         cudaMallocManaged(&input, sizeof(int) * SIZE);
         cudaMallocManaged(&output, sizeof(int) * SIZE);
@@ -34,16 +54,23 @@ int main(void){
                 input[i] = 1;
         }
 
+
+	double start = get_clock();
         //run kernel on elements on the GPU
         prefixsum<<<1,SIZE>>>(input, output);
+	double middle = get_clock();
+
 
         cudaDeviceSynchronize();
+	double end = get_clock();
         printf("%s\n", cudaGetErrorString(cudaGetLastError()));
 
-        for (int i=0;i<SIZE;i++){
+	 printf("start: %f, middle: %f, end: %f", start, middle, end);
+
+       /* for (int i=0;i<SIZE;i++){
                 printf("%d ", output[i]);
         }
-        printf("\n");
+        printf("\n");*/
         //check for errors
 //      float maxError = 0;
 //      for (int i=0;i<SIZE;i++){
